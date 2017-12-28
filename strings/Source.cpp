5a0fa27a79c4275bee56067f1f46b3e6f62b5c12
Farhan5381 <farhanmahmood433@gmail.com>
2017-12-28 13:25:03 +0500

    Created a simple question answer console program for practicing file I\O opereations

--- strings/Source.cpp
+++ strings/Source.cpp
@@ -1,93 +1,131 @@
 #include <conio.h>
+#include <fstream>
 
-void print( const char* s )
+namespace chili
 {
-	for( ; *s != 0; s++ )
+	void print( const char* s )
 	{
-		_putch( *s );
-	}
-}
-
-void read( char* buf,int maxSize )
-{
-	const char* const pEnd = buf + maxSize;
-	for( char c = _getch(); c != 13 && (buf + 1 < pEnd); c = _getch(),buf++ )
-	{
-		_putch( c );
-		*buf = c;
-	}
-	*buf = 0;
-}
-
-int str2int( const char* s )
-{
-	// scan to start point
-	const char* p = s;
-	for( ; *p >= '0' && *p <= '9'; p++ );
-	p--;
-
-	int val = 0;
-	int place = 1;
-	// convert place values and accumulate
-	for( ; p >= s; p-- )
-	{
-		val += (*p - '0') * place;
-		place *= 10;
+		for( ; *s != 0; s++ )
+		{
+			_putch( *s );
+		}
 	}
 
-	return val;
-}
-
-int fib( int n )
-{
-	if( n < 2 )
+	void read( char* buf, int maxSize )
 	{
-		return n;
+		const char* const pEnd = buf + maxSize;
+		for( char c = _getch(); c != 13 && ( buf + 1 < pEnd ); c = _getch(), buf++ )
+		{
+			_putch( c );
+			*buf = c;
+		}
+		*buf = 0;
 	}
-	return fib( n - 1 ) + fib( n - 2 );
-}
 
-void strrev( char* pl )
-{
-	// scan to start point
-	char* pr = pl;
-	for( ; *pr != 0; pr++ );
-	pr--;
-
-	for( ; pl < pr; pl++,pr-- )
+	int str2int( const char* s )
 	{
-		const char temp = *pl;
-		*pl = *pr;
-		*pr = temp;
-	}
-}
+		// scan to start point
+		const char* p = s;
+		for( ; *p >= '0' && *p <= '9'; p++ );
+		p--;
 
-void int2str( int val,char* buf,int size )
-{
-	char* const pStart = buf;
-	char* const pEnd = buf + size;
-	for( ; val > 0 && (buf + 1 < pEnd); val /= 10,buf++ )
-	{
-		*buf = '0' + val % 10;
+		int val = 0;
+		int place = 1;
+		// convert place values and accumulate
+		for( ; p >= s; p-- )
+		{
+			val += ( *p - '0' ) * place;
+			place *= 10;
+		}
+
+		return val;
 	}
-	*buf = 0;
-	strrev( pStart );
+
+	int fib( int n )
+	{
+		if( n < 2 )
+		{
+			return n;
+		}
+		return fib( n - 1 ) + fib( n - 2 );
+	}
+
+	void strrev( char* pl )
+	{
+		// scan to start point
+		char* pr = pl;
+		for( ; *pr != 0; pr++ );
+		pr--;
+
+		for( ; pl < pr; pl++, pr-- )
+		{
+			const char temp = *pl;
+			*pl = *pr;
+			*pr = temp;
+		}
+	}
+
+	void int2str( int val, char* buf, int size )
+	{
+		char* const pStart = buf;
+		char* const pEnd = buf + size;
+		for( ; val > 0 && ( buf + 1 < pEnd ); val /= 10, buf++ )
+		{
+			*buf = '0' + val % 10;
+		}
+		*buf = 0;
+		strrev( pStart );
+	}
 }
 
 int main()
 {
-	print( "Enter a number: " );
-	char answer[69];
-	read( answer,69 );
+	std::ifstream blob( "blob.txt" );
+	if( !blob )
+	{
+		chili::print( "Failed to open file: " );
+		while( !_kbhit() );
+	}
 
-	print( "\nFibonacci number <" );
-	print( answer );
-	print( "> is: " );
+	for( char c = blob.get(); blob.good(); c = blob.get() )
+	{
+		_putch( c );
+	}
+	_putch( '\n' );
 
-	const int fibNumber = fib( str2int( answer ) );
-	int2str( fibNumber,answer,69 );
-	print( answer );
+	if( blob.bad() )
+	{
+		chili::print( "I/O error occoured while opening the file!" );
+	}
+	else if( blob.eof() )
+	{
+		chili::print( "Successfully reached the end of file!" );
+	}
+	else
+	{
+		chili::print( "Some error occoured while reading the file!" );
+	}
 
+	std::ofstream ans( "ans2blob.txt" );
+	chili::print( "\nWrite your answer: " );
+	for( char c = _getch(); c != 13; c = _getch())
+	{
+		if( c != '\b' )
+		{
+			_putch( c );
+			ans.put( c );
+		}
+		else
+		{
+			_putch( c );
+			ans.seekp( -1, std::ios::cur );
+		}
+	}
+	chili::print( "\nYour answer was stored in file \"ans2blob.txt\"" );
+
+
+
+	chili::print( "\nPress any key to exit..." );
 	while( !_kbhit() );
 	return 0;
 }
